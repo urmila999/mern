bae07c57e6f4a7fa32c29f8cf20dfa7b1d697cd9
urmila999 <159011765+urmila999@users.noreply.github.com>
2024-07-27 15:50:01 +0545

    Add files via upload
    f commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center text-lg p-4">Developed By Urmila Gurung</p>
+    </footer>
+  );
+};
+
+export default Footer;
